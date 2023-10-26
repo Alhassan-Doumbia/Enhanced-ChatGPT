fe2310a7dcfaf7ee0b8e6e4a0b8b45f6d7d49485
Doumbia Alhassan <hassan4cod@gmail.com>
2023-10-26 18:05:14 +0000

    après intégration de la fonction de reconnaissane vocale , le principal problème observé est que celle ci est uniquement disponable au premier chargement de la page

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-// On va d'abord gerer la possibilité d'afficher les requêtes de l'utilisateur à l'écran 
-let sendinIcon=document.getElementById("sendinIcon");
-let chat_content=document.querySelector(".chat-content");
+const sendIcon = document.getElementById("sendinIcon");
+const chatContent = document.querySelector(".chat-content");
 
-function showUserQuery(){
-    let Query=document.getElementById("userInputField").value;
-    let userQuery=document.createElement("li")
-    userQuery.innerText=`User : ${Query}`;
-    chat_content.appendChild(userQuery); 
+function showUserQuery() {
+    const userInput = document.getElementById("userInputField").value;
+    const userQuery = document.createElement("li");
+    userQuery.innerText = userInput;
+    chatContent.appendChild(userQuery);
 }
-//On va gérer le clic sur le bouton pour envoyer une requête
-sendinIcon.addEventListener("click",showUserQuery())
 
-let inputField=document.getElementById("userInputField")//Pour pouvoir utiliser la touche entré
-inputField.addEventListener('keypress',function (event) {
-    if(event.keyCode===13){showUserQuery();event.preventDefault();
-         inputField.value="";return false;//Pour vider le contenu du bail
-        }//Comparer l'event(paamètre)  au code de la touche entrée , si oui , on execute notre bail
-})
+function speechRecognition() {
+    const userQuery = document.createElement("li");
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.continuous = false;
+    recognition.lang = "en-US"//"fr-FR";
+    recognition.interimResults = true;
 
-// On va utiliser quasiment la même methode mais cette fois ci on va faire entrer une API dans le game 
+    let currentTranscription = '';
+    let timeout;
 
+    recognition.onresult = function (event) {
+        const interimTranscript = event.results[0][0].transcript;
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(() => {
+            currentTranscription += interimTranscript + ' ';
+            userQuery.innerText = currentTranscription;
+            chatContent.appendChild(userQuery);
+        }, 1000);
+    }
+    recognition.start();
+}
+// Réagir au clic sur l'icône d'envoi
+sendIcon.addEventListener("click", showUserQuery());
+const micIcon = document.querySelector("#mic-icon");
+// Réagir au clic sur l'icône du microphone
+micIcon.addEventListener('click', speechRecognition());
+
+const inputField = document.getElementById("userInputField");
+
+// Réagir à la pression de la touche "Entrée" pour envoyer la requête
+inputField.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13) {
+        showUserQuery();
+        event.preventDefault();
+        inputField.value = "";
+        return false;
+    }
+});
